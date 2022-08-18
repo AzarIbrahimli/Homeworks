0144867c6ca018832b9c5e866a6692dd28621003
Azar Ibrahimli <azer.ibrahimli.98@gmail.com>
2022-08-18 20:25:59 +0400

    Initial

---
+++ src/main/java/homework8/DomesticCat.java
@@ -0,0 +1,14 @@
+package homework8;
+
+
+public class DomesticCat extends Pet implements FoulAnimals {
+    @Override
+    public void respond() {
+        System.out.println("Hello,owner.I am " + getNickname() + ".I miss you! ");
+    }
+
+    @Override
+    public void foul() {
+        System.out.println("I need to cover it");
+    }
+}
---
+++ src/main/java/homework8/Man.java
@@ -0,0 +1,17 @@
+package homework8;
+
+
+import java.util.Map;
+
+public class Man extends Human implements Petgreet {
+    Pet pet;
+    public Man(String humanName, String humanSurname, int humanBirth, int humanIq, Map<DayofWeek, String> schedule) {
+        super(humanName, humanSurname, humanBirth, humanIq, schedule);
+    }
+    public void greetPet() {
+        System.out.println("Hi, darling");
+    }
+    public void Workout(){
+        System.out.println("I do pull-up every day");
+    }
+}
